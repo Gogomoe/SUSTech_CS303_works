85d35153882ec54db2983c345b9b3c5c21fd2459
Gogo <gogo81745.moe@gmail.com>
2020-09-17 21:53:31 +0800

    add: alpha-beta algorithm

--- project1_reversi/ai.py
+++ project1_reversi/ai.py
@@ -4,6 +4,8 @@ import time
 
 from typing import List, Tuple
 
+INFINITY = 1000000000
+
 COLOR_BLACK = -1
 COLOR_WHITE = 1
 COLOR_NONE = 0
@@ -16,37 +18,34 @@ class AI(object):
     # chessboard_size, color, time_out passed from agent
     def __init__(self, chessboard_size: int, color: int, time_out: int):
         self.chessboard_size = chessboard_size
-        # You are white or black
         self.color = color
-        # the max time you should use, your algorithm's run time must not exceed the time limit.
         self.time_out = time_out
-        # You need add your decision into your candidate_list.
-        # System will get the end of your candidate_list as your decision.
         self.candidate_list = []
 
     # The input is current chessboard.
     def go(self, chessboard: np.ndarray):
-        # Clear candidate_list, must do this step
         self.candidate_list.clear()
-        # ==================================================================
-        # Write your algorithm here
-        # Here is the simplest sample:Random decision
 
         actions = self.actions(chessboard, self.color)
 
         self.candidate_list.extend(actions)
-        if len(actions) != 0:
-            self.candidate_list.append(random.choice(actions))
+        if len(actions) <= 1:
+            return
 
-        # ==============Find new pos========================================
-        # Make sure that the position of your decision in chess board is empty.
-        # If not, the system will return error.
-        # Add your decision into candidate_list, Records the chess board
-        # You need add all the positions which is valid
-        # candidate_list example: [(3,3),(4,4)]
-        # You need append your decision at the end of the candidate_list,
-        # we will choose the last element of the candidate_list as the position you choose
-        # If there is no valid position, you must return a empty list.
+        depth = 4
+
+        result_move = actions[0]
+        new_chessboard = self.make_move(chessboard, actions[0], self.color)
+        result_max = -self.alpha_beta(new_chessboard, -INFINITY, INFINITY, -self.color, depth)
+
+        for action in actions[1:]:
+            new_chessboard = self.make_move(chessboard, action, self.color)
+            result = -self.alpha_beta(new_chessboard, -INFINITY, INFINITY, -self.color, depth)
+            if result > result_max:
+                result_max = result
+                result_move = action
+
+        self.candidate_list.append(result_move)
 
     def actions(self, chessboard: np.ndarray, color: int) -> List[Tuple[int, int]]:
         def check_point(sy: int, sx: int) -> bool:
@@ -71,3 +70,73 @@ class AI(object):
                     result.append((i, j))
 
         return result
+
+    def alpha_beta(self, chessboard: np.ndarray, alpha: int, beta: int, color: int, depth: int) -> int:
+        sign = {self.color: 1, -self.color: -1}
+        result_max = -INFINITY
+        if depth <= 0:
+            return sign[color] * self.evaluate(chessboard)
+        if not self.can_move(chessboard, color):
+            if not self.can_move(chessboard, -color):
+                return sign[color] * self.evaluate(chessboard)
+            return -self.alpha_beta(chessboard, -beta, -alpha, -color, depth)
+
+        for move in self.actions(chessboard, color):
+            new_chessboard = self.make_move(chessboard, move, color)
+            val = -self.alpha_beta(new_chessboard, -beta, -alpha, -color, depth - 1)
+            if val > alpha:
+                if val >= beta:
+                    return val
+                alpha = max(alpha, val)
+            result_max = max(result_max, val)
+        return result_max
+
+    def evaluate(self, chessboard: np.ndarray) -> int:
+        weight = [
+            6,  # 0 corner
+            -3,  # 1 corner danger
+            -6,  # 2 more danger
+            2,  # 3 point
+            2,  # 4 mobility
+        ]
+
+        result = 0
+        sign = {self.color: 1, -self.color: -1, 0: 0}
+
+        for (i, j) in [(0, 0), (-1, 0), (0, -1), (-1, -1)]:
+            result += sign[chessboard[i, j]] * weight[0]
+
+        for (i, j) in [(0, 1), (1, 0), (0, -2), (-2, 0), (1, -1), (-1, 1), (-1, -2), (-2, -1)]:
+            result += sign[chessboard[i, j]] * weight[1]
+
+        for (i, j) in [(1, 1), (1, -2), (-2, 1), (-2, -2)]:
+            result += sign[chessboard[i, j]] * weight[2]
+
+        for i in range(self.chessboard_size):
+            for j in range(self.chessboard_size):
+                result += sign[chessboard[i, j]] * weight[3]
+
+        result += len(self.actions(chessboard, self.color)) * weight[4]
+
+        return result
+
+    def can_move(self, chessboard: np.ndarray, color: int) -> bool:
+        return len(self.actions(chessboard, color)) > 0
+
+    def make_move(self, chessboard: np.ndarray, move: Tuple[int, int], color: int):
+        new = chessboard.copy()
+        sy, sx = move
+        for direct in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+            y = sy + direct[0]
+            x = sx + direct[1]
+            if 0 <= x < self.chessboard_size and 0 <= y < self.chessboard_size and new[y, x] != -color:
+                continue
+            while 0 <= x < self.chessboard_size and 0 <= y < self.chessboard_size and new[y, x] == -color:
+                y += direct[0]
+                x += direct[1]
+            if 0 <= x < self.chessboard_size and 0 <= y < self.chessboard_size and new[y, x] == color:
+                while y != sy or x != sx:
+                    y -= direct[0]
+                    x -= direct[1]
+                    new[y, x] = color
+        return new
