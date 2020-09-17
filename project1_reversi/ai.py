583bdc4a89ef057497ed0066f71980c085713b4a
Gogo <gogo81745.moe@gmail.com>
2020-09-17 15:12:29 +0800

    add: random algorithm

--- project1_reversi/ai.py
+++ project1_reversi/ai.py
@@ -2,6 +2,8 @@ import numpy as np
 import random
 import time
 
+from typing import List, Tuple
+
 COLOR_BLACK = -1
 COLOR_WHITE = 1
 COLOR_NONE = 0
@@ -12,7 +14,7 @@ random.seed(0)
 class AI(object):
 
     # chessboard_size, color, time_out passed from agent
-    def __init__(self, chessboard_size, color, time_out):
+    def __init__(self, chessboard_size: int, color: int, time_out: int):
         self.chessboard_size = chessboard_size
         # You are white or black
         self.color = color
@@ -23,14 +25,19 @@ class AI(object):
         self.candidate_list = []
 
     # The input is current chessboard.
-    def go(self, chessboard):
+    def go(self, chessboard: np.ndarray):
         # Clear candidate_list, must do this step
         self.candidate_list.clear()
         # ==================================================================
         # Write your algorithm here
         # Here is the simplest sample:Random decision
-        idx = np.where(chessboard == COLOR_NONE)
-        idx = list(zip(idx[0], idx[1]))
+
+        actions = self.actions(chessboard, self.color)
+
+        self.candidate_list.extend(actions)
+        if len(actions) != 0:
+            self.candidate_list.append(random.choice(actions))
+
         # ==============Find new pos========================================
         # Make sure that the position of your decision in chess board is empty.
         # If not, the system will return error.
@@ -40,3 +47,27 @@ class AI(object):
         # You need append your decision at the end of the candidate_list,
         # we will choose the last element of the candidate_list as the position you choose
         # If there is no valid position, you must return a empty list.
+
+    def actions(self, chessboard: np.ndarray, color: int) -> List[Tuple[int, int]]:
+        def check_point(sy: int, sx: int) -> bool:
+            if chessboard[sy, sx] != 0:
+                return False
+            for direct in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                y = sy + direct[0]
+                x = sx + direct[1]
+                if 0 <= x < self.chessboard_size and 0 <= y < self.chessboard_size and chessboard[y, x] != -color:
+                    continue
+                while 0 <= x < self.chessboard_size and 0 <= y < self.chessboard_size and chessboard[y, x] == -color:
+                    y += direct[0]
+                    x += direct[1]
+                if 0 <= x < self.chessboard_size and 0 <= y < self.chessboard_size and chessboard[y, x] == color:
+                    return True
+            return False
+
+        result = []
+        for i in range(self.chessboard_size):
+            for j in range(self.chessboard_size):
+                if check_point(i, j):
+                    result.append((i, j))
+
+        return result
